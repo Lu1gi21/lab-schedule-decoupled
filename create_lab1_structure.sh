7aab8bf3542d8acccd34f83255a1aa60600c1772
Mady Teel <mteel0010@cs060.cs.ourcloud.ou.edu>
2025-03-11 03:37:49 +0000

    Iteration on lab structure shell, also fleshing out cascading scripts.

--- create_lab1_structure.sh
+++ create_lab1_structure.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ./lab
 touch big-red-button.sh
+touch ./lab/run-all-tests.sh
 mkdir ./lab/docs/
 mkdir ./lab/utils/ # any additional tooling and code that provide support
 mkdir ./lab/objectives/
@@ -19,16 +20,23 @@ for obj in a b c d e; do
     mkdir -p ./lab/objectives/objective-$obj
     touch ./lab/objectives/objective-$obj/{run-all-task-tests.sh,run-all-verify.sh,run-all-measure.sh,run-clean-all.sh}
 
-    for task in $(seq -w 0 $((task_counts[$obj]-1))); do
-        mkdir -p ./lab/objectives/objective-$obj/task$task
-        touch ./lab/objectives/objective-$obj/task$task/{run-all-variant-tests.sh,run-all-verify.sh,run-all-measure.sh,run-clean-all.sh}
+    for task_num in $(seq -w 0 $((task_counts[$obj]-1))); do
+        task=$(printf "task%02d" $((10#$task_num)))  # Format task number as "task00" to keep convention across variants
+        mkdir -p ./lab/objectives/objective-$obj/$task
+        touch ./lab/objectives/objective-$obj/$task/{run-all-variant-tests.sh,run-all-verify.sh,run-all-measure.sh,run-clean-all.sh}
 
         for var in 00 01; do
-            mkdir -p ./lab/objectives/objective-$obj/task${task}/var${var}
-            touch ./lab/objectives/objective-$obj/task${task}/var${var}/{run-all-tests.sh,run-all-verify.sh,run-all-measure.sh,run-clean-all.sh}
+            mkdir -p ./lab/objectives/objective-$obj/$task/var$var
+            touch ./lab/objectives/objective-$obj/$task/var$var/{run-all-tests.sh,run-all-verify.sh,run-all-measure.sh,run-clean-all.sh}
         done
     done
 done
 
 touch ./lab/objectives/objective-{b,c,d}/run-unit-tests.sh # run unit tests and coverage on all of the generators (use ./lab/examples/basic-generator for ideas on unit testing and coverage)
 
+for obj in b c d; do
+    mkdir -p ./lab/objectives/objective-$obj/unit-tests/{input,expected-output,tests}
+    touch ./lab/objectives/objective-$obj/unit-tests/input/input00.txt
+    touch ./lab/objectives/objective-$obj/unit-tests/expected-output/output00.out
+    touch ./lab/objectives/objective-$obj/unit-tests/tests/task00-var00-test00.x
+done
