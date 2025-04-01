241e1baa6e63dd60e6ea4c88292c9cc9c4dbc1c8
ColemanBixler <cbixlerpersonal@gmail.com>
2025-04-01 17:34:43 +0000

    Fixed Conflicting Tests

--- lab/objectives/objective-a/task03/var01/run-all-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Running tests for Objective $1, Task $2, Variant $3..."
-bash run-all-measure.sh
-bash run-all-verify.sh
-bash run-clean-all.sh
-echo "Tests completed for Objective $1, Task $2, Variant $3."
--- lab/objectives/objective-a/task03/var04/run-all-measure.sh
+++ lab/objectives/objective-a/task03/var04/run-all-measure.sh
@@ -1,11 +1,2 @@
 #! /bin/bash
 #
-# Top level script to run verification in this directory.
-# Note that the "-C" flag specifies the directory of the make file.
-
-# NOTE: Adjust FILE_REF and FILE_TST to fit your task's needs.
-#make -C ../.. measure-all FILE_REF=./examples/basic/baseline.c FILE_TST=./examples/basic/baseline.c
-make -C ../.. measure-all FILE_REF=./examples/basic/baseline.c FILE_TST=./examples/basic/test_var000.c
-
-# NOTE: For every variant in the task (or test) make sure to run it in here if it is relevant.
-    
