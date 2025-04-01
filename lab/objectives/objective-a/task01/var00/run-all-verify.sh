4baf6a0ebbb3d4ab1ab9701c49895a92a434e7bf
Mady Teel <mteel0010@cs060.cs.ourcloud.ou.edu>
2025-04-01 06:44:32 +0000

    Obj-A T01 - Supporting shells roughly in place...updated .c files to have same naming conventions as other regions.

--- lab/objectives/objective-a/task01/var00/run-all-verify.sh
+++ lab/objectives/objective-a/task01/var00/run-all-verify.sh
@@ -3,8 +3,10 @@
 # Top level script to run verification in this directory.
 # Note that the "-C" flag specifies the directory of the make file.
 
-# NOTE: Adjust FILE_REF and FILE_TST to fit your task's needs.
-#make -C ../.. measure-verifier FILE_REF=./examples/basic/baseline.c FILE_TST=./examples/basic/baseline.c
-make -C ../.. measure-verifier FILE_REF=./examples/basic/baseline.c FILE_TST=./examples/basic/test_var000.c
+# Hardcoded file name to verify in this variant
+FILE_TST=ssa-conv4x3_qrij_unrollvar00_ijqr.c
+FILE_REF=../../../task00/ssa-versions/ssa_baseline.c
+
+make -C ../.. measure-verifier FILE_REF="$FILE_REF" FILE_TST="$FILE_TST"
 
 # NOTE: For every variant in the task (or test) make sure to run it in here if it is relevant.
