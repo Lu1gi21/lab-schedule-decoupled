cb270475df097613503a48ae468b22a26688e501
ColemanBixler <cbixlerpersonal@gmail.com>
2025-04-08 18:11:25 +0000

    Fixing Run File Operation

--- lab/objectives/objective-d/task05/var00/run.sh
+++ lab/objectives/objective-d/task05/var00/run.sh
@@ -1,6 +1,5 @@
-# echo "ENTER OPERATION (Note: must be exact operation name; e.g. enter 'conv_4x3_ijqr')"
-# READ OP
-OP="conv_4x3_ijqr"
+echo "ENTER OPERATION (Note: must be exact operation name; e.g. enter 'conv_4x3_ijqr')"
+READ OP
 echo "Note: this is for 2 interchanges and 2 splits. Used example is split_ijqr_2_interchange_qirj."
 SCHEDULE="split_ijqr_2_interchange_qirj"
 
