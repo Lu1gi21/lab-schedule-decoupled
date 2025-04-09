a622ef139c5d6eed41fe74807f4c0fd36bf254e3
Mady Teel <mteel0010@cs060.cs.ourcloud.ou.edu>
2025-04-09 04:25:22 +0000

    To a good saving spot before dicey testing...

--- lab/objectives/objective-a/task03/run-all-measure.sh
+++ lab/objectives/objective-a/task03/run-all-measure.sh
@@ -5,13 +5,18 @@ if [[ "$1" == "--full" ]]; then
     FULL=true
 fi
 
+bash ../../generate-baselines.sh
+bash ./interchange-versions/generate-all-interchanges.sh
+
 for variant in var*/; do
     echo "Running: measuring $(basename "$variant")..."
 
     if $FULL; then
         make -C "$variant" measure-all
+        make -C "$variant" plot-all  # <-- plot both baseline + variant
     else
         make -C "$variant" measure-verifier
         make -C "$variant" measure-performance
+        make -C "$variant" plot-variant  # <-- just plot the variant
     fi
 done
--- lab/objectives/objective-a/task03/run-all-variant-tests.sh
+++ lab/objectives/objective-a/task03/run-all-variant-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+bash ./interchange-versions/generate-all-interchanges.sh
 bash ./run-clean-all.sh
 bash ./run-all-verify.sh
-bash ./run-all-measure.sh
+bash ./run-all-measure.sh  --full
